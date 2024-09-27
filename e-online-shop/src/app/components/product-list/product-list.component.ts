b6d2d3da6506c8ad24dfc2b636eb0ff7b18f4ed0
eljona406 <eljona.gjoka@fti.edu.al>
2024-09-28 00:30:09 +0200

    Add add to cart functionality to product cards

--- e-online-shop/src/app/components/product-list/product-list.component.ts
+++ e-online-shop/src/app/components/product-list/product-list.component.ts
@@ -4,6 +4,8 @@ import { Product } from '../../shared/models/product.model';
 import { ProductService } from '../../shared/services/products.service';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { CartService } from '../../shared/services/cart.service';
 
 @Component({
 	selector: 'app-product-list',
@@ -13,9 +15,22 @@ import { MatButtonModule } from '@angular/material/button';
 	styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-	products: Product[] = [];
+	public products: Product[] = [];
 
-	constructor(private productService: ProductService) {
+	constructor(
+		private productService: ProductService,
+		private router: Router,
+		private cartService: CartService
+	) {
 		this.products = this.productService.getProducts();
+	}
+
+	public viewProductDetails(productId: number) {
+		this.router.navigate(['/product', productId]);
+	}
+
+	public addToCart(product: Product) {
+		this.cartService.addToCart(product, 1);
+		alert(`${product.name} added to the cart`);
 	}
 }
