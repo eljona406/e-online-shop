2824e81ef3cad1f968bb1e143f407d40ca9bd880
eljona406 <eljona.gjoka@fti.edu.al>
2024-09-27 17:04:41 +0200

    add angular materials,/material-cards component

--- e-online-shop/src/app/components/product-list/product-list.component.ts
+++ e-online-shop/src/app/components/product-list/product-list.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../shared/models/product.model';
 import { ProductService } from '../../shared/services/products.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
 	selector: 'app-product-list',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, MatCardModule, MatButtonModule],
 	templateUrl: './product-list.component.html',
 	styleUrls: ['./product-list.component.css'],
 })
