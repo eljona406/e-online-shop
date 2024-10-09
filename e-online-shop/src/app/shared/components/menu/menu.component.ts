e5c13b3ae0dab35f7036d3d384e413c25cb835bc
eljona406 <eljona.gjoka@fti.edu.al>
2024-10-09 16:15:00 +0100

    Update menu design

--- e-online-shop/src/app/shared/components/menu/menu.component.ts
+++ e-online-shop/src/app/shared/components/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
 
 @Component({
 	selector: 'app-menu',
@@ -9,8 +10,23 @@ import { RouterModule } from '@angular/router';
 	templateUrl: './menu.component.html',
 	styleUrl: './menu.component.css',
 })
-export class MenuComponent {
+export class MenuComponent implements  OnInit{
 	public isMenuOpen = false;
+	public username: string | null = null;
+
+	constructor(
+		private router: Router
+	) {}
+
+	public ngOnInit(): void {
+		if (typeof window !== 'undefined') {
+			const username = localStorage.getItem('user_firstName');
+
+			if (username) {
+				this.username = username;
+			}
+		}
+	}
 
 	public openMenu() {
 		this.isMenuOpen = true;
@@ -18,5 +34,25 @@ export class MenuComponent {
 
 	public closeMenu() {
 		this.isMenuOpen = false;
+	}
+	onOverlayClick() {
+		// Close the menu if the click is outside the menu content
+		this.closeMenu();
+	  }
+	  onOverlayKeydown(event: KeyboardEvent) {
+		if (event.key === 'Enter' || event.key === ' ') {
+		  this.closeMenu();
+		}
+	  }
+
+	onLogout() {
+		if (typeof window !== 'undefined') {
+			localStorage.removeItem('auth_token');
+			localStorage.removeItem('user_email');
+			localStorage.removeItem('user_firstName');
+			localStorage.removeItem('user_surname');
+		}
+		this.username = null;
+		this.router.navigate(['/login']);
 	}
 }
