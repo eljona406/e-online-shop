85595767c767d10ddf08d009c4943cc5162f3748
eljona406 <eljona.gjoka@fti.edu.al>
2024-10-14 11:38:57 +0100

    Update  carousel behaviour to avoid autoscroll and add swipe/dots

--- e-online-shop/src/app/shared/components/carousel/carousel.component.ts
+++ e-online-shop/src/app/shared/components/carousel/carousel.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input, OnInit, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
 import { CommonModule } from '@angular/common';
 import { Observable, Subscription } from 'rxjs';
 import { Product } from '../../models/product.model';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
 	selector: 'app-carousel',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, CarouselModule],
 	templateUrl: './carousel.component.html',
 	styleUrls: ['./carousel.component.css'],
 })
@@ -16,32 +16,41 @@ export class CarouselComponent implements OnInit, OnDestroy {
 	@Input() category = '';
 	public filteredProducts: Product[] = [];
 	public currentImageIndex = 0;
-	public intervalId!: number;
 	public imagesToShow = 6;
 	public productSubscription!: Subscription;
 	public startX = 0;
 	public endX = 0;
 
+	customOptions: OwlOptions = {
+		loop: false,
+		margin: 10,
+		dots: true,
+		nav: false,
+		responsive: {
+			0: { items: 1 },
+			600: { items: 3 },
+			1000: { items: 6 },
+		},
+		touchDrag: true,
+		mouseDrag: true,
+		autoWidth: false,
+		center: false,
+	};
+
+	get totalGroups(): number {
+		return Math.ceil(this.filteredProducts.length / this.imagesToShow);
+	}
+
 	public get displayedProducts() {
 		const numberOfProducts = this.filteredProducts.length;
 
-		// If there are fewer products than `imagesToShow`, return all of them
 		if (numberOfProducts <= this.imagesToShow) {
 			return this.filteredProducts;
 		}
 
-		// Handle circular view
-		const endIndex = this.currentImageIndex + this.imagesToShow;
-
-		if (endIndex <= numberOfProducts) {
-			// Normal case, no wrapping required
-			return this.filteredProducts.slice(this.currentImageIndex, endIndex);
-		} else {
-			// Wrapping case: take products from the end and beginning
-			const part1 = this.filteredProducts.slice(this.currentImageIndex, numberOfProducts);
-			const part2 = this.filteredProducts.slice(0, endIndex % numberOfProducts);
-			return [...part1, ...part2]; // Combine both slices
-		}
+		const startIndex = this.currentImageIndex * this.imagesToShow;
+		const endIndex = Math.min(startIndex + this.imagesToShow, numberOfProducts);
+		return this.filteredProducts.slice(startIndex, endIndex);
 	}
 
 	constructor(@Inject(PLATFORM_ID) private platformId: object) {}
@@ -49,7 +58,6 @@ export class CarouselComponent implements OnInit, OnDestroy {
 	public ngOnInit() {
 		if (this.products) {
 			this.productSubscription = this.products.subscribe((products) => {
-				// Filter by category if provided
 				if (this.category) {
 					this.filteredProducts = products.filter(
 						(product) => product.category === this.category
@@ -57,23 +65,11 @@ export class CarouselComponent implements OnInit, OnDestroy {
 				} else {
 					this.filteredProducts = products;
 				}
-
-				if (isPlatformBrowser(this.platformId)) {
-					this.startAutoScroll(); // Start auto-scroll only in the browser
-				}
 			});
 		}
 	}
 
-	public startAutoScroll() {
-		this.intervalId = window.setInterval(() => {
-			this.currentImageIndex = (this.currentImageIndex + 1) % this.filteredProducts.length;
-		}, 3000);
-	}
-
-	// Clean up the interval and subscription
 	public ngOnDestroy() {
-		clearInterval(this.intervalId);
 		if (this.productSubscription) {
 			this.productSubscription.unsubscribe();
 		}
@@ -88,22 +84,32 @@ export class CarouselComponent implements OnInit, OnDestroy {
 		this.handleSwipe();
 	}
 
-	public nextImage() {
-		this.currentImageIndex = (this.currentImageIndex + 1) % this.filteredProducts.length;
-	}
-
-	public prevImage() {
-		this.currentImageIndex =
-			(this.currentImageIndex - 1 + this.filteredProducts.length) %
-			this.filteredProducts.length;
-	}
-
 	public handleSwipe() {
 		const swipeThreshold = 50;
 		if (this.startX - this.endX > swipeThreshold) {
-			this.nextImage();
+			this.nextImageGroup();
 		} else if (this.endX - this.startX > swipeThreshold) {
-			this.prevImage();
+			this.prevImageGroup();
 		}
+	}
+
+	public nextImageGroup() {
+		if (this.currentImageIndex < this.totalGroups - 1) {
+			this.currentImageIndex++;
+		} else {
+			this.currentImageIndex = this.totalGroups - 1; // Prevent going out of bounds
+		}
+	}
+
+	public prevImageGroup() {
+		if (this.currentImageIndex > 0) {
+			this.currentImageIndex--;
+		} else {
+			this.currentImageIndex = 0; // Prevent going out of bounds
+		}
+	}
+
+	public goToGroup(index: number) {
+		this.currentImageIndex = index;
 	}
 }
